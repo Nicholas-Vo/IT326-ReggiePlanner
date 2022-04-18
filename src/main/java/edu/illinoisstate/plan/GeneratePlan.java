61129dc15a09562d9f1a35b969398fa096ad3d1e
Nick <nvoss@me.com>
2022-04-18 15:29:47 -0500

    began generate plan

--- src/main/java/edu/illinoisstate/plan/GeneratePlan.java
+++ src/main/java/edu/illinoisstate/plan/GeneratePlan.java
@@ -4,32 +4,39 @@ import edu.illinoisstate.UserAccount;
 import edu.illinoisstate.course.Course;
 import edu.illinoisstate.database.Database;
 
+import javax.swing.JButton;
+import javax.swing.JTextField;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GeneratePlan {
     private final Database db = Database.getInstance();
     private final List<Course> completedCourses = new ArrayList<>();
+    private final JButton addBtn = new JButton();
+    private final JTextField inputField = new JTextField();
 
     public GeneratePlan(UserAccount user) {
         generate();
     }
 
     private void generate() {
-        Course course = db.getCourseByID("IT279");
+        addBtn.addActionListener(e -> {
+            String input = inputField.getText();
 
-        if (course == null) {
-            System.out.println("Error, course not found.");
-        } else {
+            if (db.getCourseByID(input) == null) {
+                //JOptionPane.showMessageDialog("Error doesn't exist");
+                return;
+            }
+
+            Course course = db.getCourseByID(input);
             completedCourses.add(course);
-            // Send "successfully added course" message
-        }
+            // course added!
+            inputField.setText("");
+        });
 
         // Now we have a list of courses the student has taken,
         // use this list to generate a plan
         // ...
-
-
     }
 
 }
